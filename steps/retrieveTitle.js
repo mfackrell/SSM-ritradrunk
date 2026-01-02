99214d1f79c8fb658574709e301d82c030a4e5eb
mfackrell <mfackrell@contract-cfo.com>
2026-01-02 10:25:34 -0600

    Update retrieveTitle.js

--- steps/retrieveTitle.js
+++ steps/retrieveTitle.js
@@ -1,19 +1,9 @@
-import { getSheetValues } from "../lib/googleSheets.js";
+import { getFirstTitle } from "../lib/googleSheets.js";
 
 export async function retrieveTitle() {
-  const spreadsheetId = "1M0sAzon8VPBqWVETCbanyFSe6zqb_VCtkEisjQtkao8";
-  const range = "Sheet1!A2:A2";
-
-  const values = await getSheetValues(spreadsheetId, range);
-
-  const title = values?.[0]?.[0];
-
-  if (!title) {
-    throw new Error("No title found in Sheet1!A2");
-  }
-
+  const title = await getFirstTitle();
   console.log("Retrieved title:", title);
-
   return title;
 }
+
 
